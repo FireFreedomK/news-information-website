5ac9e611e70575a96e022576532336708df271c3
tu <liulangdeerhu@qq.com>
2020-05-09 19:27:16 +0800

    5.redis存储配置

--- info/__init__.py
+++ info/__init__.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from redis import StrictRedis
+
 from config import config_dict
 
+
+# 定义redis_store变量
+redis_store = None
+redis_store=StrictRedis()
 
 #定义工厂方法
 def create_app(config_name):
@@ -11,6 +17,12 @@ def create_app(config_name):
 
     #加载配置类
     app.config.from_object(config)
+
+    # 创建redis对象
+    global redis_store  # global将局部变量声明为一个全局的
+    redis_store = StrictRedis(host=config.REDIS_HOST,
+                              port=config.REDIS_PORT,
+                              decode_responses=True)
 
     # 将首页蓝图index_bp,注册到app中
     from info.modules.index import index_bp
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -3,4 +3,8 @@ from . import index_bp
 
 @index_bp.route('/')
 def index():
+    from info import redis_store
+    # 测试redis
+    #参数1：key ，参数2：value
+    redis_store.set('name', 'laoli')
     return 'index'
