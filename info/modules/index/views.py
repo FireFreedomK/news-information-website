9f0fc33594048b9b90100e9db1cab3575567ba39
tu <liulangdeerhu@qq.com>
2020-05-11 15:26:15 +0800

    11.网站logo显示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,21 +1,15 @@
 import logging
-from flask import render_template
+from flask import render_template,current_app
 
 from . import index_bp
 
 
 @index_bp.route('/')
 def show_index():
-    from info import redis_store
-    # 测试redis
-    #参数1：key ，参数2：value
-    #redis_store.set('name', 'laoli')
-
-    # 使用日志记录方法loggin进行输出可控
-    # logging.debug("输入调试信息")
-    # logging.info("输入详细信息")
-    # logging.warning("输入警告信息")
-    # logging.error("输入错误信息")
-
     return render_template('news/index.html')
 
+#处理网站logo
+@index_bp.route('/favicon.ico')
+def get_web_log():
+
+    return current_app.send_static_file('news/favicon.ico')
