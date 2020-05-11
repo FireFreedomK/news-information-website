696484f4304eaa3e46c0e112492306fd26e595d7
tu <liulangdeerhu@qq.com>
2020-05-11 11:08:18 +0800

    6.日志信息集成

--- info/__init__.py
+++ info/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 from redis import StrictRedis
 
 from config import config_dict
-
+import logging
+from logging.handlers import RotatingFileHandler
 
 # 定义redis_store变量
 redis_store = None
@@ -12,11 +13,19 @@ redis_store=StrictRedis()
 def create_app(config_name):
     app = Flask(__name__)
 
+    config = config_dict.get(config_name)
+
+    # 调用日志方法,记录程序运行信息
+    log_file(config.LEVEL_NAME)
+
     #根据传入的配置类名称,取出对应的配置类
     config = config_dict.get(config_name)
 
     #加载配置类
     app.config.from_object(config)
+
+    #调用日志方法，记录程序运行信息
+    log_file(config.LEVEL_NAME)
 
     # 创建redis对象
     global redis_store  # global将局部变量声明为一个全局的
@@ -29,3 +38,16 @@ def create_app(config_name):
     app.register_blueprint(index_bp)
 
     return app
+
+
+def log_file(LEVEL_NAME):
+    # 设置日志的记录等级,常见的有四种,大小关系如下: DEBUG < INFO < WARNING < ERROR
+    logging.basicConfig(level=LEVEL_NAME)  # 调试debug级,一旦设置级别那么大于等于该级别的信息全部都会输出
+    # 创建日志记录器，指明日志保存的路径、每个日志文件的最大大小、保存的日志文件个数上限
+    file_log_handler = RotatingFileHandler("logs/log", maxBytes=1024 * 1024 * 100, backupCount=10)
+    # 创建日志记录的格式 日志等级 输入日志信息的文件名 行数 日志信息
+    formatter = logging.Formatter('%(levelname)s %(filename)s:%(lineno)d %(message)s')
+    # 为刚创建的日志记录器设置日志记录格式
+    file_log_handler.setFormatter(formatter)
+    # 为全局的日志工具对象（flask app使用的）添加日志记录器
+    logging.getLogger().addHandler(file_log_handler)
--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,3 +1,5 @@
+import logging
+
 from . import index_bp
 
 
@@ -6,5 +8,11 @@ def index():
     from info import redis_store
     # 测试redis
     #参数1：key ，参数2：value
-    redis_store.set('name', 'laoli')
+    #redis_store.set('name', 'laoli')
+
+    # 使用日志记录方法loggin进行输出可控
+    logging.debug("输入调试信息")
+    logging.info("输入详细信息")
+    logging.warning("输入警告信息")
+    logging.error("输入错误信息")
     return 'index'
