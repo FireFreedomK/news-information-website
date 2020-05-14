de75ca3a432dd5f738325776d9285cc77a1bd6f4
tu <liulangdeerhu@qq.com>
2020-05-14 20:36:33 +0800

    09.首页-新闻列表展示

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,9 +1,88 @@
 import logging
 from flask import render_template,current_app, session,jsonify
+from flask import request
 
 from . import index_bp
 from info.models import User,News,Category
 from info.utils.response_code import RET
+
+
+# 首页新闻列表
+# 请求路径: /newslist
+# 请求方式: GET
+# 请求参数: cid,page,per_page
+# 返回值: data数据
+@index_bp.route('/newslist')
+def newslist():
+    """
+    1. 获取参数
+    2. 参数类型转换
+    3. 分页查询
+    4. 获取到分页对象中的属性,总页数,当前页,当前页的对象列表
+    5. 将对象列表转成字典列表
+    6. 携带数据,返回响应
+    :return:
+    """
+    # 1. 获取参数
+    cid = request.args.get("cid","1")
+    page = request.args.get("page","1")
+    per_page = request.args.get("per_page","10")
+
+    # 2. 参数类型转换
+    try:
+        page = int(page)
+        per_page = int(per_page)
+    except Exception as e:
+        page = 1
+        per_page = 10
+
+    # 3. 分页查询
+    try:
+        """
+        #判断新闻的分类是否为1
+        if cid == "1":
+            paginate = News.query.filter().order_by(News.create_time.desc()).paginate(page,per_page,False)
+        else:
+            paginate = News.query.filter(News.category_id == cid).order_by(News.create_time.desc()).paginate(page,per_page,False)
+        """
+
+        """
+        # 改装,判断新闻的分类是否为1
+        filters = ""
+        if cid != "1":
+            filters = (News.category_id == cid)
+        paginate = News.query.filter(filters).order_by(News.create_time.desc()).paginate(page, per_page, False)
+        """
+
+        # 改装,判断新闻的分类是否为1
+        filters = []
+        if cid != "1":
+            filters.append(News.category_id == cid)
+        paginate = News.query.\
+            filter(*filters).\
+            order_by(News.create_time.desc()).\
+            paginate(page, per_page, False)
+
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="获取新闻失败")
+
+    # 4. 获取到分页对象中的属性,总页数,当前页,当前页的对象列表
+    totalPage = paginate.pages
+    currentPage = paginate.page
+    items = paginate.items
+
+    # 5. 将对象列表转成字典列表
+    news_list = []
+    for news in items:
+        news_list.append(news.to_dict())
+
+    # 6. 携带数据,返回响应
+    return jsonify(errno=RET.OK,
+                   errmsg="获取新闻成功",
+                   totalPage=totalPage,
+                   currentPage=currentPage,
+                   newsList=news_list)
 
 
 @index_bp.route('/')
